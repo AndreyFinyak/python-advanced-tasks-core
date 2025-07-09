9cefc39163a069815f239dc0b4471028b7404a1b
AndreyFinyak <afinyak@yahoo.com>
2025-07-09 17:42:32 +0300

    Задание 2 обновлено. Добавлены методы push, pop, peek в класс Stack

--- task-02/stack.py
+++ task-02/stack.py
@@ -1,6 +1,6 @@
 class Stack:
     def __init__(self, obj):
-        if not isinstance(obj, (list, set, tuple, dict,)):
+        if not isinstance(obj, (list, set, tuple,)):
             raise TypeError("Stack obj must be a list, set, tuple, or dict")
         self._stack = obj
 
@@ -8,7 +8,15 @@ class Stack:
         iter_obj = self._stack[::-1]
         return iter(iter_obj)
 
+    def push(self, item):
+        self._stack.append(item)
 
-stack = Stack([1, 2, 3])
-for item in stack:
-    print(item)
+    def pop(self):
+        if not self._stack:
+            raise IndexError("pop from empty stack")
+        return self._stack.pop()
+
+    def peek(self):
+        if not self._stack:
+            raise IndexError("peek from empty stack")
+        return self._stack[-1]
